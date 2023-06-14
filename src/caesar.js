2e08dab48d312d4a729662d84682ea96ab149417
Matt Lewis <118700125+iammattlewis@users.noreply.github.com>
2023-06-13 20:31:07 -0400

    added decode mode and helper function (shifter)

--- src/caesar.js
+++ src/caesar.js
@@ -7,17 +7,31 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
-    if (
-      !shift || 
-      shift < -25 || 
-      shift > 25) {
+    if (!shift || shift < -25 || shift > 25 || typeof input !== "string") {
         return false;
       }
+    // activates decode mode, which inverts the shift  
+    if (encode === false) {
+      shift = shift * -1;
+      return input
+        .toLowerCase()
+        .split("")
+        .map((character) => shifter(character, shift))
+        .join("")
+    }    
+    function shifter(character, shift) {
+      // alphabet array for the key
+      const key = "abcdefghijklmnopqrstuvwxyz".split(""); 
+      if (!character.match(/[a-z]/)) return character;
+
+      let index = key.indexOf(character);
+      let shifted = (((index + shift) % 26) + 26) % 26;
+      return key[shifted];
+    }  
   }
 
   return {
     caesar,
   };
 })();
-
 module.exports = { caesar: caesarModule.caesar };
