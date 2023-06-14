84c9f0e736a3398edf3e2e9c2d26e616a4b3d210
Matt Lewis <118700125+iammattlewis@users.noreply.github.com>
2023-06-14 12:49:15 -0400

    first commit for polybius

--- src/polybius.js
+++ src/polybius.js
@@ -5,10 +5,47 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+  const keys = {
+    mainKey: createKey("alpha"),
+    coordinateKey: createKey("coord"),
+  };
 
   function polybius(input, encode = true) {
-    // your solution code here
+    try {
+      if (!input.length) throw new Error("Input cannot be empty.");
+      return input
+        .split(" ")
+        .map((word) => iterateWord(word, encode, keys))
+        .join(" ");
+    } catch (error) {
+      return false;
+    }
   }
+
+  // Helper functions
+
+  function iterateWord(word, encode, {mainKey, coordinateKey}) {
+    if (encode)
+      return word
+        .toLowerCase()
+        .split("")
+        .map((letter) => mapMatrix(letter, mainKey, coordinateKey))
+        .join("");
+
+    if (word.length % 2 !== 0)
+      throw new Error("Coordinates come in pairs. Decryption cannot occur with an odd numbered total.")
+    
+    let output = "";
+    for (let char = 0; char < word.length; char += 2) {
+      const column = word[char];
+      const row = word[char +1];
+      const code = `${column}${row}`;
+      output += mapMatrix(code, coordinateKey, mainKey);
+    }
+    return output;
+  }
+
+  
 
   return {
     polybius,
