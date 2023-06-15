f3ca30f4b607f8a6f9027346c94f3cbf29d1f22b
Matt Lewis <118700125+iammattlewis@users.noreply.github.com>
2023-06-15 15:09:25 -0400

    added new functions to polybius

--- src/polybius.js
+++ src/polybius.js
@@ -6,8 +6,8 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
   const keys = {
-    mainKey: createKey("alpha"),
-    coordinateKey: createKey("coord"),
+    mainKey: createKey("main"),
+    coordinateKey: createKey("coordinate"),
   };
 
   function polybius(input, encode = true) {
@@ -24,28 +24,71 @@ const polybiusModule = (function () {
 
   // Helper functions
 
-  function iterateWord(word, encode, {mainKey, coordinateKey}) {
+  function iterateWord(word, encode, { mainKey, coordinateKey }) {
+    // encode
     if (encode)
       return word
         .toLowerCase()
         .split("")
         .map((letter) => mapMatrix(letter, mainKey, coordinateKey))
         .join("");
-
+    
+    // decode
     if (word.length % 2 !== 0)
       throw new Error("Coordinates come in pairs. Decryption cannot occur with an odd numbered total.")
     
     let output = "";
     for (let char = 0; char < word.length; char += 2) {
-      const column = word[char];
-      const row = word[char +1];
-      const code = `${column}${row}`;
+      const col = word[char];
+      const row = word[char + 1];
+      const code = `${col}${row}`;
       output += mapMatrix(code, coordinateKey, mainKey);
     }
     return output;
   }
 
+  function mapMatrix(input, fromKey, toKey) {
+    const coord = findCoordinate(input, fromKey);
+    if (!coord) throw new Error (`${input} is not valid. Please re-enter a valid input.`);
+    const row = coord[0];
+    const col = coord[1];
+    return toKey[row][col];
+  }
   
+  function findCoordinate(input, key) {
+    if (input === "i" || input === "j") input = "(i/j)";
+    for (let row = 0; row < 5; row++)
+      for (let col = 0; col < 5; col++) {
+        if (key[row][col] === input) return [row, col];
+      }
+    return false;  
+  }
+
+  function createKey(type = "main", size = 5) {
+    const grid = [];
+    for (let row = 0; row < size; row++) {
+      const thisRow = [];
+      for (let col = 0; col < size; col++) {
+        type === "main"
+        ? thisRow.push(mainIndex(row, col, size))
+        : thisRow.push(coordIndex(row, col));
+      }
+      grid.push(thisRow);
+    }
+    return grid;
+  }
+
+  function mainIndex(row, col, size) {
+    const number = row * size + col;
+    let charCode = number + 97;
+    if (charCode === 105) return "(i/j)";
+    const shift = charCode > 105 ? 1 : 0;
+    return String.fromCharCode(charCode + shift);
+  }
+  function coordIndex(row, col) {
+    return `${col + 1}${row + 1}`;
+  }
+
 
   return {
     polybius,
