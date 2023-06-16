34c07f1edb9ec9cf68369380a3b6c3d4511100b3
Matt Lewis <118700125+iammattlewis@users.noreply.github.com>
2023-06-16 14:13:39 -0400

    added positive shift testing for ecryption

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,3 +1,26 @@
 // Write your tests here!
-const caesar = require("../src/caesar");
-const {expect} = require("chai");
+const expect = require("chai").expect;
+const { caesar } = require("../src/caesar");
+
+
+describe("Caesar Shift", () => {
+  describe("Encryption", () => {
+    // positive shift
+    it("encrypts single word", () => {
+      const input = "thinkful";
+      const shift = 3;
+      const expected = "wklqnixo";
+      const actual = caesar(input, shift);
+      expect(actual).to.be.equal(expected);
+    });
+
+    it("encrypts multiple words, phrases, and sentences", () => {
+      const input = "hello mother leopard"
+      const shift = 2;
+      const expected = "jgnnq oqvjgt ngqrctf";
+      const actual = caesar(input, shift);
+      expect(actual).to.be.equal(expected);
+    });
+  });
+  
+})
