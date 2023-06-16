f16e6d6cbd25c00e53555305ac00bd9bb0ffac3f
Matt Lewis <118700125+iammattlewis@users.noreply.github.com>
2023-06-16 19:59:59 -0400

    added negative shift testing for encryption

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -8,19 +8,56 @@ describe("Caesar Shift", () => {
     // positive shift
     it("encrypts single word", () => {
       const input = "thinkful";
-      const shift = 3;
-      const expected = "wklqnixo";
+      const shift = 2;
+      const expected = "vjkpmhwn";
       const actual = caesar(input, shift);
       expect(actual).to.be.equal(expected);
     });
 
-    it("encrypts multiple words, phrases, and sentences", () => {
+    it("encrypts multiple words", () => {
       const input = "hello mother leopard"
-      const shift = 2;
-      const expected = "jgnnq oqvjgt ngqrctf";
+      const shift = 1;
+      const expected = "ifmmp npuifs mfpqbse";
+      const actual = caesar(input, shift);
+      expect(actual).to.be.equal(expected);
+    });
+
+    it("encrypts when wrapping around the alphabet", () => {
+      const input = "oxygenize zooprophylaxis xylophone"
+      const shift = 9;
+      const expected = "xghpnwrin ixxyaxyqhujgrb ghuxyqxwn"
       const actual = caesar(input, shift);
       expect(actual).to.be.equal(expected);
     });
   });
+
+
+  // negative shift
+  describe("Negative Shift", () => {
+    it("encrypts a single word", () => {
+      const input = "thinkful";
+      const shift = -8;
+      const expected = "lzafcxmd";
+      const actual = caesar(input, shift);
+      expect(actual).to.be.equal(expected);
+    });
+
+    it("encrypts multiple words", () => {
+      const input = "hello mother leopard";
+      const shift = -12;
+      const expected = "vszzc achvsf zscdofr";
+      const actual = caesar(input, shift);
+      expect(actual).to.be.equal(expected);
+    });
+
+    it("ecrypts when wrapping around the alphabet", () => {
+      const input = "new canopies arise";
+      const shift = -10;
+      const expected = "dum sqdefyui qhyiu";
+      const actual = caesar(input, shift);
+      expect(actual).to.be.equal(expected);
+    });
+
+  })
   
 })
